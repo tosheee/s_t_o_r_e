1e8a976bdb685c3c154e27f2cbe4776c033dcb90
Todor Chakarov <tosheee@abv.bg>
2017-10-18 08:18:17 +0300

    Fix form create product

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -58,20 +58,22 @@
             </label>
 
             <label>
-                Available: <input type="radio" name="description[product_status]" value="наличен" checked>
-                Order: <input type="radio" name="description[product_status]" value="по поръчка">
-                Not available: <input type="radio" name="description[product_status]" value="не наличен">
+                <span style="margin: 0;">Availability: </span>
+                <input type="radio" name="description[product_status]" value="наличен" checked> Available:
+                <input type="radio" name="description[product_status]" value="по поръчка"> Order:
+                <input type="radio" name="description[product_status]" value="не наличен"> Not available:
             </label>
-
+            <br>
             <label>
                 <span>Price:</span>
                 <input type="text" name="description[price]" value="" id="admin_product_description" class="label-values"/>
             </label>
 
             <label>
-                BGN: <input type="radio" name="description[currency]" value="лв." checked>
-                EUR: <input type="radio" name="description[currency]" value="euro">
-                USD: <input type="radio" name="description[currency]" value="usd">
+                <span style="margin: 0;">Currency:</span>
+                 <input type="radio" name="description[currency]" value="лв." checked> BGN:
+                 <input type="radio" name="description[currency]" value="euro"> EUR:
+                 <input type="radio" name="description[currency]" value="usd">  USD:
             </label>
             <br>
 
@@ -205,6 +207,44 @@
                 e.preventDefault(); $(this).parent('div.fields label').remove(); x--;
             });
         });
+
+
+
+
+
+        $('[id^="btnO"]').click(function() {
+            var notchecked = $('input[type="radio"][name="menucolor"]').not(':checked');
+            $('.navbar.'+notchecked.val()).toggleClass('navbar-default navbar-inverse');
+            notchecked.prop("checked", true);
+            $(this).parent().find('a').each(function() {
+                if($(this).attr('id') == 'btnOn'){
+                    $(this).toggleClass('active btn-success btn-default');
+                } else {
+                    $(this).toggleClass('active btn-danger btn-default');
+                }
+
+            });
+            doChange(notchecked);
+        });
+
+        $('input[type="radio"][name="menucolor"]').change(function() {
+            doChange(this);
+        });
+
+        function doChange(object){
+            if($(object).val() == "navbar-default"){
+                $('#btnOn').removeClass('active');
+                $('#btnOn .glyphicon-ok').css('opacity','0');
+                $('#btnOff .glyphicon-remove').css('opacity','1');
+                $('#btnOff').focus();
+            }
+            if($(object).val() == "navbar-inverse"){
+                $('#btnOff').removeClass('active');
+                $('#btnOff .glyphicon-remove').css('opacity','0');
+                $('#btnOn .glyphicon-ok').css('opacity','1');
+                $('#btnOn').focus();
+            }
+        }
     </script>
 
     @include('admin.admin_partials.admin_menu_bottom')
