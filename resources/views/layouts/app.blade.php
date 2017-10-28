8e5bb05ff57a37cd72cfab7a81368e9559bb4dd2
Todor Chakarov <tosheee@abv.bg>
2017-10-28 16:18:27 +0300

    Add links for style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,43 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+        <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <link href="https://stark-shelf-54930.herokuapp.com/css/app.css" rel="stylesheet" type="text/css" >
 
-    <!-- Styles -->
-    <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+        <script type="text/javascript" src="https://stark-shelf-54930.herokuapp.com/js/app.js"></script>
+    </head>
 
-    <script type="text/javascript" src="{{ URL::asset('js/app.js') }}"></script>
+    <body>
+        <div id="app">
 
-</head>
-<body>
-    <div id="app">
+            <header>
+                @include('partials.horizontal_nav_bar')
+            </header>
 
-        <header>
-            @include('partials.horizontal_nav_bar')
-        </header>
-
-        <div id="wrapper">
-            <div class="container-fluid">
-                <div class="row">
-                     @yield('content') </div>
+            <div id="wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                         @yield('content') </div>
+                    </div>
                 </div>
             </div>
-        </div>
 
 
-        <footer>
-            @include('partials.footer')
-        </footer>
-
-
-
-
-</body>
+            <footer>
+                @include('partials.footer')
+            </footer>
+    </body>
 </html>
