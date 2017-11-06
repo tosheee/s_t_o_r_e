b311c27cb16ad6221874f5e2d8e9ade36da716bf
Todor Chakarov <tosheee@abv.bg>
2017-11-06 08:30:29 +0200

    Remove slider

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -5,11 +5,9 @@
 
     <div class="col-md-2"> @include('partials.v_nav_bar') </div>
     <div class="col-md-9">
-        @include('partials.slider_index')
-
-        <br/><br/>
+        <br/>
+        <br/>
         <div class="row">
-
             @foreach($products as $product)
                 <?php $descriptions = json_decode($product->description, true); ?>
                     <div class="col-sm-4">
