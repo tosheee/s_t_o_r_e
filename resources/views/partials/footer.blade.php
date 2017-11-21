ada3383e0bdc7aa79910668f6adbaee6eac41f1f
Todor Chakarov <tosheee@abv.bg>
2017-11-21 08:48:33 +0200

    Remove domain in query

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -10,7 +10,7 @@
          }
         $.ajax({
             method: "POST",
-            url: "https://stark-shelf-54930.herokuapp.com/store/add-to-cart/?product_id=" + idProduct + "&product_quantity=" + quantityProduct,
+            url: "/store/add-to-cart/?product_id=" + idProduct + "&product_quantity=" + quantityProduct,
             data: { "_token": "{{ csrf_token() }}" },
             success: function( new_cart ) {
 
