edc026fea4c3c3918a24ec18e4e5783a7581ccd2
Todor Chakarov <tosheee@abv.bg>
2017-11-21 19:19:45 +0200

    Test https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,10 +17,16 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         //Schema::defaultStringLength(191);
+       // if(env('APP_ENV') !== 'local')
+        //{
+          //  $url->forceSchema('https');
+        //}
+        //URL::forceScheme('https');
         if(env('APP_ENV') !== 'local')
         {
-            $url->forceSchema('https');
+            URL::forceScheme('https');
         }
+
     }
 
     /**
